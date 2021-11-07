75ddb0ac9287250b43e7fa140935d5ea8a2d44ae
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-07 13:13:07 +0100

    Feature: Using extends to provide dynamic polymorphism option.

---
+++ src/com/umer/oop/ploymorphism/dynamically/extension/Bird.java
@@ -0,0 +1,11 @@
+package com.umer.oop.ploymorphism.dynamically.extension;
+
+import com.umer.oop.ploymorphism.dynamically.Animal;
+
+public class Bird extends Animal{
+	
+	public void eat() {
+		System.out.println("This bird eats seeds.");
+	}
+
+}
