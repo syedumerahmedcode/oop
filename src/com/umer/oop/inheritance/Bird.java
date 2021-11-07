a5aace99efb5d14b741f6fe1920675a4806c0891
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-07 09:42:08 +0100

    Feature: Started creating an example for inheritance - work in progress.

---
+++ src/com/umer/oop/inheritance/Bird.java
@@ -0,0 +1,15 @@
+package com.umer.oop.inheritance;
+
+public class Bird {
+
+	public String reproduction = "Egg";
+	public String outerCovering = "Feathers";
+
+	public void flyingUp() {
+		System.out.println("Flying up ...");
+	}
+
+	public void flyingDown() {
+		System.out.println("Flying down ...");
+	}
+}
