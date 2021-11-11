5e58e8a7fe9909a973d093fcb608437926d0a2fe
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-11 13:59:29 +0100

    Feature: Added methods in Footballplayer class.

--- src/com/umer/oop/aggregation/FootballPlayer.java
+++ src/com/umer/oop/aggregation/FootballPlayer.java
@@ -2,4 +2,32 @@ package com.umer.oop.aggregation;
 
 public class FootballPlayer {
 
+	private String name;
+	private Football football;
+	
+	public FootballPlayer(String name, Football football) {
+		super();
+		this.name = name;
+		this.football = football;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Football getFootball() {
+		return this.football;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFootball(Football football) {
+		this.football = football;
+	}
+	
+	
+	
+	
 }
